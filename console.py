b9ade5570acee30cf1a324c5ef622d6ed931f4bf
Mourad Azouga <mourad-azouga@outlook.fr>
2024-02-20 18:53:40 +0100

    Fixed the classes holder

--- console.py
+++ console.py
@@ -49,7 +49,7 @@ class HBNBCommand(cmd.Cmd):
             if len(class_name) == 0:
                 print("** class name missing **")
                 return
-            if class_name and class_name not in self.valid_classes:
+            if class_name and class_name not in self.__classes:
                 print("** class doesn't exist **")
                 return
 

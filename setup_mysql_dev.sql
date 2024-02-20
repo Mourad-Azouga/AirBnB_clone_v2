b238e91cdfc3f942f127c3a48ab433e6da1f5dfa
Mourad-Azouga <mourad-azouga@outlook.fr>
2024-02-20 11:03:39 -0800

    Updated the sql for creating the databse and the user and removed the duplicates of IDENTIFIED WITH in the perms granting queries

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,15 +4,7 @@
 --  3-All perms go to the user
 --  4-SELECT pers for performance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED WITH 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED WITH 'hbnb_dev_pwd';
-GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED WITH 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;

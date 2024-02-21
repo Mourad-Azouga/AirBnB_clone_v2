540bba9b4a19be6d8345d22061e43ad2d4315cdd
Mourad Azouga <mourad-azouga@outlook.fr>
2024-02-21 20:24:17 +0100

    Added all the tests, updated all the necessary files for task 7

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy import Column, DateTime, String
 
+#This will cause issues when used in Vscode because of the insalations (Mourad)
 
+Base = declarative_base()
 class BaseModel:
-    """A base class for all hbnb models"""
+    """A base class for all hbnb models
+    Attributes:
+        id (sqlalchemy String): the basemodel id
+        created_at (sqlalchemy DateTime): the date of creation
+        updated_at (sqlalchemy DateTime): the date of update
+    """
+        
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = self.updated_at = datetime.now()
-        storage.new(self)
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -26,9 +38,9 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
@@ -39,3 +51,6 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+    def delete(self):
+        """Deletes the current instnance from the storage"""
+        models.storage.delete(self)

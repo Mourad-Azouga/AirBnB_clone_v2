8150781a31ec1ed4c9300d6d39157fa88c2281fe
Mourad Azouga <mourad-azouga@outlook.fr>
2024-02-21 20:30:41 +0100

    Second update

--- models/city.py
+++ models/city.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""Defines the City class."""
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
+from sqlalchemy import ForeignKey
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
-class User(BaseModel, Base):
-    """Represents a user for a MySQL database.
+class City(BaseModel, Base):
+    """Represents a city for a MySQL database.
 
-    Inherits from SQLAlchemy Base and links to the MySQL table users.
+    Inherits from SQLAlchemy Base and links to the MySQL table cities.
 
     Attributes:
-        __tablename__ (str): The name of the MySQL table to store users.
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
+        __tablename__ (str): The name of the MySQL table to store Cities.
+        name (sqlalchemy String): The name of the City.
+        state_id (sqlalchemy String): The state id of the City.
     """
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
--- models/state.py
+++ models/state.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from os import getenv
+"""Defines the State class."""
 import models
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, City, String
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class
+    """Represents a state for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
     Attributes:
-        __tablename__ (sqlalchemy): Table name
-        name (sqlalchemy String): State name
-        cities (sqlalchemt Relationship): City name within the State
-        """
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City",  backref="state", cascade="delete")

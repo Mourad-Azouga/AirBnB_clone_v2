c39265be2ee0d15b43e2ff4890ccc0b24398187e
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-22 16:57:53 +0000

    automated pusher

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -14,9 +14,23 @@ app = Flask(__name__)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
+
 @app.route('/hbnb', strict_slashes=False)
 def hello():
+    """Displays HBNB!'"""
     return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_cool(text):
+    """Displays C + input text, changes _ to  """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+@app.route('/python/<text>', strict_slashes=False)
+def c_is_cool(text):
+    """Displays python + input text, changes _ to  """
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")

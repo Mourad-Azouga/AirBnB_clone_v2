2c45b34d82733f510b2591ba9106000fe34c4801
Mourad Azouga <mourad-azouga@outlook.fr>
2024-03-22 16:53:43 +0000

    automated pusher

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """Displays HBNB!'"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_cool(text):
+    """Displays C + input text, changes _ to  """
+    text = text.replace("_", " ")
+    return 'C f{text}'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
